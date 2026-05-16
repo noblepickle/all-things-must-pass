e7eee0895c51b543c0e26e720280547d7e82e2ea
thenoblepickle <aubrunnerkorbinian3@gmail.com>
2026-05-16 15:17:09 +0200

    Adding text styling and a scroll trigger for the cover

--- main.js
+++ main.js
@@ -2,12 +2,31 @@ const { gsap, ScrollTrigger } = window;
 
 gsap.registerPlugin(ScrollTrigger);
 
+
+// Cover ----------------------------------
 const cover = document.querySelector('img');
 
-gsap.to(cover, { 
-    rotate: 360, 
-    duration: 20,
-    repeat: -1,
-    ease: 'none',
+const originalDuration = 20;
+const maxDuration = 80;
+
+const spin = gsap.to(cover, {
+  rotate: 360,
+  duration: originalDuration,
+  repeat: -1,
+  ease: 'none'
 });
 
+ScrollTrigger.create({
+  trigger: "body",
+  start: "top top",
+  end: "bottom top",
+  scrub: true,
+  onUpdate: (self) => {
+    const p = self.progress * 7;
+    const newDuration = originalDuration + (maxDuration - originalDuration) * p;
+    spin.timeScale(originalDuration / newDuration);
+
+    // cover.style.opacity = String(1 - (p * 5));
+    gsap.set(cover, { y: p * 1500});
+  },
+}); 
