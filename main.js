4819f35773f36c3f97a097ee01a94bf85f4b3d21
thenoblepickle <aubrunnerkorbinian3@gmail.com>
2026-05-16 16:31:16 +0200

    Adding spotify button

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 // Cover ----------------------------------
-const cover = document.querySelector('img');
+const cover = document.querySelector('.cover');
 
 const originalDuration = 20;
 const maxDuration = 80;
@@ -30,3 +30,19 @@ ScrollTrigger.create({
     gsap.set(cover, { y: p * 1500});
   },
 }); 
+
+// Content ---------------------------------------------
+const p = document.querySelectorAll('.track');
+
+p.forEach(element => {
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: element,
+      start: "center +=1000",
+      end: "+=600",
+      scrub: true,
+    }
+  })
+  .fromTo(element, { opacity: 0 }, { opacity: 1, duration: 5 })
+  .to(element, { opacity: 0, duration: 5 });
+});
